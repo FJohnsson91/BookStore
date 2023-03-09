15abc194d97efc1059bbcfacd4a0da2aed0b250d
Fredrik Johnsson <fredrik.johnsson0029@stud.hkr.se>
2023-03-09 13:11:18 +0100

    Added filter options

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,49 @@ import { getJSON } from './utilities/getJSON.js'
 import '../scss/style.scss'
 import * as bootstrap from 'bootstrap'
 
-let books, categories = []
+let books,
+  chosenCategoryFilter = 'All',
+  chosenSortOption,
+  categories = []
 
 async function start() {
   books = await getJSON('./data/books.json')
   getCategories()
   addFilters()
-  displayBooks()
+  addSortingOptions()
+  sortByAuthor(books)
+  displayBooks(books)
 }
 
 function getCategories() {
   let withDuplicates = books.map(book => book.category)
   categories = [...new Set(withDuplicates)]
+  categories.sort()
+}
+
+function sortByAuthor(books) {
+  books.sort(({ author: aLastName }, { author: bLastName }) =>
+    aLastName > bLastName ? 1 : -1);
+}
+
+function addSortingOptions() {
+  document.querySelector('.sortingOptions').innerHTML = /*html*/`
+    <label><span>Sort by:</span>
+      <select class="sortOption">
+        <option>Author</option>
+        <option>Price</option>
+      </select>
+    </label>
+  `
+  document.querySelector('.sortOption').addEventListener('change', event => {
+    chosenSortOption = event.target.value
+    displayBooks()
+  })
+}
+
+function sortByprice(books) {
+  books.sort(({ price: aAge }, { price: bAge }) =>
+    aAge > bAge ? 1 : -1);
 }
 
 function addFilters() {
@@ -24,11 +55,21 @@ function addFilters() {
         ${categories.map(category => `<option>${category}</option>`).join('')}
       </select>
     </label>
-  `;
+  `
+  document.querySelector('.categoryFilter').addEventListener('change', event => {
+    chosenCategoryFilter = event.target.value
+    displayBooks()
+  })
 }
 
 function displayBooks() {
-  let htmlArray = books.map(({
+  let filteredBooks = books.filter(
+    ({ category }) => chosenCategoryFilter === 'All'
+      || chosenCategoryFilter === category
+  )
+  if (chosenSortOption === 'Author') { sortByAuthor(filteredBooks) }
+  if (chosenSortOption === 'Price') { sortByprice(filteredBooks) }
+  let htmlArray = filteredBooks.map(({
     title, author, category, price
   }) => /*html*/`
     <div class="book">
